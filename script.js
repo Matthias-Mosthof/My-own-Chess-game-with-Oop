fb8791b2c7e7406bc1af3d802cfeb82bec8d0a0b
Matthias Mosthof <matthias.mosthof@gmail.com>
2022-11-18 00:59:35 +0100

    adding-dynamic-eventListener

--- script.js
+++ script.js
@@ -138,10 +138,13 @@ const chessBoard = [
 function renderWhereIsBishopF1() {
   let currentPosition;
   let fieldNumber = 0;
+
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-bishop-f1") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-bishop-f1") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   // Wo steht er
@@ -219,9 +222,11 @@ function renderWhereIsRookH1() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-rook-h1") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-rook-h1") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   // Wo steht er
@@ -286,9 +291,11 @@ function renderWhereIsKnightG1() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-knight-g1") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-knight-g1") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   // Wo steht er
@@ -327,7 +334,6 @@ function renderWhereIsKnightG1() {
       currentPosition.attributes.class.nodeValue.slice(-1) !== "8" &&
       chessBoard[fieldNumber + t] !== undefined
     ) {
-      console.log("hello");
       knightSees.push(chessBoard[fieldNumber + t]);
     }
   }
@@ -355,7 +361,6 @@ function renderWhereIsKnightG1() {
   // Was sieht Springer nach links links unten und rechts rechts oben
 
   for (let u = 17; u < 34; u = u + 17) {
-    console.log("hello");
     if (
       chessBoard[fieldNumber + u] !== undefined &&
       currentPosition.attributes.class.nodeValue.slice(-1) !== "8"
@@ -383,9 +388,11 @@ function renderWhereIsQueenD1() {
   let fieldNumber = 0;
   let queenSees = [];
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-queen-d1") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-queen-d1") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   for (let r = 1; r < 8; r++) {
@@ -505,9 +512,11 @@ function renderWhereIsKingE1() {
   let fieldNumber = 0;
   let kingSees = [];
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-king-e1") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-king-e1") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
 
@@ -572,9 +581,11 @@ function renderWhereIsPawnA2() {
   let fieldNumber = 0;
   let pawnSees = [];
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-a2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-a2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -588,9 +599,11 @@ function renderWhereIsPawnB2() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-b2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-b2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -605,9 +618,11 @@ function renderWhereIsPawnC2() {
   let fieldNumber = 0;
   let pawnSees = [];
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-c2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-c2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -621,9 +636,11 @@ function renderWhereIsPawnD2() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-d2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-d2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -637,9 +654,11 @@ function renderWhereIsPawnE2() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-e2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-e2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -652,9 +671,11 @@ function renderWhereIsPawnF2() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-f2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-f2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -667,9 +688,11 @@ function renderWhereIsPawnG2() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-g2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-g2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -682,9 +705,11 @@ function renderWhereIsPawnH2() {
   let currentPosition;
   let fieldNumber = 0;
   for (let i = 0; i < chessBoard.length; i++) {
-    if (chessBoard[i].children[0].attributes.id.value === "white-pawn-h2") {
-      currentPosition = chessBoard[i];
-      fieldNumber = i;
+    if (chessBoard[i].childElementCount > 0) {
+      if (chessBoard[i].children[0].attributes.id.value === "white-pawn-h2") {
+        currentPosition = chessBoard[i];
+        fieldNumber = i;
+      }
     }
   }
   return {
@@ -703,13 +728,13 @@ function whatAnyPawnSees(style) {
     pawnSees.push(chessBoard[fieldNumber + 1]);
   }
   //return pawnSees;
-  console.log(currentPosition);
   for (i in pawnSees) {
     pawnSees[i].style.boxShadow = `${insetColorMovePossibilty}`;
-    if (style.length > 1) {
+    if (style === "none") {
       pawnSees[i].style.boxShadow = "none";
     }
   }
+  return pawnSees;
 }
 
 // Alle weißen Figuren als Variable speichern
@@ -754,6 +779,7 @@ let currentPosition;
 let lastPosition;
 let currentMovedPiece;
 let currentActiveSide;
+//let lastPossibleMoves = [];
 let result;
 
 const state = {
@@ -782,6 +808,7 @@ const state = {
       pieceMovedFromPosition: lastPosition,
       pieceMovedToPosition: currentPosition,
       currentActiveSide: currentActiveSide,
+      lastPossibleMoves: [],
     },
   ],
   End: [
@@ -869,20 +896,39 @@ function renderMove() {
 
   //state.End.forEach((move) => console.log(move));
 
+  let allPossibleMovesBeforeNewPosition =
+    state.currentMove[0].lastPossibleMoves;
+
+  //console.log(allPossibleMovesBeforeNewPosition[0].style);
+
   let positionNow = state.currentMove[0].pieceMovedToPosition;
+  console.log(positionNow);
   let pieceNow = state.currentMove[0].currentMovedPiece;
 
-  if (positionNow === "fieldb3") {
+  if (positionNow === "field-b3") {
+    //allPossibleMovesBeforeNewPosition[0].style.boxShadow = "none";
+
     fieldb3.append(pieceNow);
+
     //moveCounter++;
   }
 
-  if (positionNow === "fieldc3") {
-    fieldc3.append(pieceNow);
+  if (positionNow === "field-b4") {
+    fieldb4.append(pieceNow);
+
     //moveCounter++;
   }
-  if (positionNow === "fieldc4") {
-    fieldc4.append(pieceNow);
+
+  if (positionNow === "field-b5") {
+    fieldb5.append(pieceNow);
+    //moveCounter++;
+  }
+  if (positionNow === "field-b6") {
+    fieldb6.append(pieceNow);
+    //moveCounter++;
+  }
+  if (positionNow === "field-b7") {
+    fieldb7.append(pieceNow);
     //moveCounter++;
   }
 
@@ -906,8 +952,8 @@ function pieceDeactivateActivate() {
 }
 
 pawnB2.addEventListener("click", () => {
-  pieceDeactivateActivate();
-
+  let pieceIsActive = pieceDeactivateActivate();
+  let pawnSees = whatAnyPawnSees();
   let parent = pawnB2.parentElement;
   console.log(pieceIsActive);
   if (pieceIsActive === true) {
@@ -915,22 +961,32 @@ pawnB2.addEventListener("click", () => {
   } else {
     whatAnyPawnSees("none");
   }
-
-  fieldb3.addEventListener("click", () => {
-    clickCounter = 0;
-    clickCounter++;
-
+  for (let i = 0; i < pawnSees.length; i++) {
+    pawnSees[i].addEventListener("click", () => {
+      //if (clickCounter === 1) {
+      console.log(pawnSees);
+      state.currentMove[0].pieceMovedToPosition =
+        pawnSees[i].attributes[1].value;
+      state.currentMove[0].currentMovedPiece = pawnB2;
+      state.betweenStartAndEnd[0].moveAmount = moveCounter;
+      state.currentMove[0].lastMovedSide = "White";
+      state.currentMove[0].pieceMovedFromPosition = parent;
+      state.currentMove[0].lastPossibleMoves = pawnSees;
+      renderMove();
+      //}
+    });
+  }
+  /*
+  fieldb4.addEventListener("click", () => {
     if (clickCounter === 1) {
-      state.currentMove[0].pieceMovedToPosition = "fieldb3";
+      state.currentMove[0].pieceMovedToPosition = "fieldb4";
       state.currentMove[0].currentMovedPiece = pawnB2;
       state.betweenStartAndEnd[0].moveAmount = moveCounter;
       state.currentMove[0].lastMovedSide = "White";
       state.currentMove[0].pieceMovedFromPosition = parent;
       renderMove();
-      clickCounter--;
-      return;
     }
-  });
+  });*/
 });
 
 pawnC2.addEventListener("click", () => {
@@ -964,255 +1020,3 @@ function writeMoveToObjectPawnC2() {
     return moveCounter;
   }
 }
-
-//console.log(moveCounter);
-// Showing all possible moves for Each piece on the first Move
-// White Pieces
-// Pawns
-// Pawn A2 Possible Moves:
-
-const possibleA3 = document.querySelector("div:nth-child(3) > aside");
-const possibleA4 = document.querySelector("div:nth-child(4) > aside");
-
-function toggle() {
-  possibleA4.classList.toggle("possiblemove");
-  possibleA3.classList.toggle("possiblemove");
-}
-
-pawnA2.addEventListener("click", toggle);
-
-// Pawn B2 Possible Moves:
-
-const possibleB3 = document.querySelector("div:nth-child(11) > aside");
-const possibleB4 = document.querySelector("div:nth-child(12) > aside");
-
-function toggle1() {
-  possibleB3.classList.toggle("possiblemove");
-  possibleB4.classList.toggle("possiblemove");
-}
-
-pawnB2.addEventListener("click", toggle1);
-
-// Pawn C2 Possible Moves:
-
-const possibleC3 = document.querySelector("div:nth-child(19) > aside");
-const possibleC4 = document.querySelector("div:nth-child(20) > aside");
-
-function toggle2() {
-  possibleC3.classList.toggle("possiblemove");
-  possibleC4.classList.toggle("possiblemove");
-}
-
-pawnC2.addEventListener("click", toggle2);
-
-// Pawn D2 Possible Moves:
-
-const possibleD3 = document.querySelector("div:nth-child(27) > aside");
-const possibleD4 = document.querySelector("div:nth-child(28) > aside");
-
-function toggle3() {
-  possibleD3.classList.toggle("possiblemove");
-  possibleD4.classList.toggle("possiblemove");
-}
-
-pawnD2.addEventListener("click", toggle3);
-
-// Pawn E2 Possible Moves:
-
-const possibleE3 = document.querySelector("div:nth-child(35) > aside");
-const possibleE4 = document.querySelector("div:nth-child(36) > aside");
-
-function toggle4() {
-  possibleE3.classList.toggle("possiblemove");
-  possibleE4.classList.toggle("possiblemove");
-}
-
-pawnE2.addEventListener("click", toggle4);
-
-// Pawn F2 Possible Moves:
-
-const possibleF3 = document.querySelector("div:nth-child(43) > aside");
-const possibleF4 = document.querySelector("div:nth-child(44) > aside");
-
-function toggle5() {
-  possibleF3.classList.toggle("possiblemove");
-  possibleF4.classList.toggle("possiblemove");
-}
-
-pawnF2.addEventListener("click", toggle5);
-
-// Pawn G2 Possible Moves:
-
-const possibleG3 = document.querySelector("div:nth-child(51) > aside");
-const possibleG4 = document.querySelector("div:nth-child(52) > aside");
-
-function toggle6() {
-  possibleG3.classList.toggle("possiblemove");
-  possibleG4.classList.toggle("possiblemove");
-}
-
-pawnG2.addEventListener("click", toggle6);
-
-// Pawn H2 Possible Moves:
-
-const possibleH3 = document.querySelector("div:nth-child(59) > aside");
-const possibleH4 = document.querySelector("div:nth-child(60) > aside");
-
-function toggle7() {
-  possibleH3.classList.toggle("possiblemove");
-  possibleH4.classList.toggle("possiblemove");
-}
-
-pawnH2.addEventListener("click", toggle7);
-
-// Knights
-// Knight B1 Possible Moves:
-
-function toggle8() {
-  possibleA3.classList.toggle("possiblemove");
-  possibleC3.classList.toggle("possiblemove");
-}
-
-knightB1.addEventListener("click", toggle8);
-
-//Knight G1 Possible Moves:
-
-function toggle9() {
-  possibleF3.classList.toggle("possiblemove");
-  possibleH3.classList.toggle("possiblemove");
-}
-
-knightG1.addEventListener("click", toggle9);
-
-// Black Pieces
-// Pawn A7 Possible Moves
-
-const pawnA7 = document.querySelector("div:nth-child(7)> svg");
-
-const possibleA6 = document.querySelector("div:nth-child(6) > aside");
-const possibleA5 = document.querySelector("div:nth-child(5) > aside");
-
-function toggle10() {
-  possibleA6.classList.toggle("possiblemove");
-  possibleA5.classList.toggle("possiblemove");
-}
-
-pawnA7.addEventListener("click", toggle10);
-
-// Pawn B7 Possible Moves
-
-const pawnB7 = document.querySelector("div:nth-child(15)> svg");
-
-const possibleB6 = document.querySelector("div:nth-child(14) > aside");
-const possibleB5 = document.querySelector("div:nth-child(13) > aside");
-
-function toggle11() {
-  possibleB6.classList.toggle("possiblemove");
-  possibleB5.classList.toggle("possiblemove");
-}
-
-pawnB7.addEventListener("click", toggle11);
-
-// Pawn C7 Possible Moves
-
-const pawnC7 = document.querySelector("div:nth-child(23)> svg");
-
-const possibleC6 = document.querySelector("div:nth-child(22) > aside");
-const possibleC5 = document.querySelector("div:nth-child(21) > aside");
-
-function toggle12() {
-  possibleC6.classList.toggle("possiblemove");
-  possibleC5.classList.toggle("possiblemove");
-}
-
-pawnC7.addEventListener("click", toggle12);
-
-// Pawn D7 Possible Moves
-
-const pawnD7 = document.querySelector("div:nth-child(31)> svg");
-
-const possibleD6 = document.querySelector("div:nth-child(30) > aside");
-const possibleD5 = document.querySelector("div:nth-child(29) > aside");
-
-function toggle13() {
-  possibleD6.classList.toggle("possiblemove");
-  possibleD5.classList.toggle("possiblemove");
-}
-
-pawnD7.addEventListener("click", toggle13);
-
-// Pawn E7 Possible Moves
-
-const pawnE7 = document.querySelector("div:nth-child(39)> svg");
-
-const possibleE6 = document.querySelector("div:nth-child(38) > aside");
-const possibleE5 = document.querySelector("div:nth-child(37) > aside");
-
-function toggle14() {
-  possibleE6.classList.toggle("possiblemove");
-  possibleE5.classList.toggle("possiblemove");
-}
-
-pawnE7.addEventListener("click", toggle14);
-
-// Pawn F7 Possible Moves
-
-const pawnF7 = document.querySelector("div:nth-child(47)> svg");
-
-const possibleF6 = document.querySelector("div:nth-child(46) > aside");
-const possibleF5 = document.querySelector("div:nth-child(45) > aside");
-
-function toggle15() {
-  possibleF6.classList.toggle("possiblemove");
-  possibleF5.classList.toggle("possiblemove");
-}
-
-pawnF7.addEventListener("click", toggle15);
-
-// Pawn G7 Possible Moves
-
-const pawnG7 = document.querySelector("div:nth-child(55)> svg");
-
-const possibleG6 = document.querySelector("div:nth-child(54) > aside");
-const possibleG5 = document.querySelector("div:nth-child(53) > aside");
-
-function toggle16() {
-  possibleG6.classList.toggle("possiblemove");
-  possibleG5.classList.toggle("possiblemove");
-}
-
-pawnG7.addEventListener("click", toggle16);
-
-// Pawn H7 Possible Moves
-
-const pawnH7 = document.querySelector("div:nth-child(63)> svg");
-
-const possibleH6 = document.querySelector("div:nth-child(62) > aside");
-const possibleH5 = document.querySelector("div:nth-child(61) > aside");
-
-function toggle17() {
-  possibleH6.classList.toggle("possiblemove");
-  possibleH5.classList.toggle("possiblemove");
-}
-
-pawnH7.addEventListener("click", toggle17);
-
-//Knight B8 Possible Moves:
-const knightB8 = document.querySelector("div:nth-child(16) > svg");
-
-function toggle18() {
-  possibleA6.classList.toggle("possiblemove");
-  possibleC6.classList.toggle("possiblemove");
-}
-
-knightB8.addEventListener("click", toggle18);
-
-//Knight G8 Possible Moves:
-const knightG8 = document.querySelector("div:nth-child(56) > svg");
-
-function toggle19() {
-  possibleF6.classList.toggle("possiblemove");
-  possibleH6.classList.toggle("possiblemove");
-}
-
-knightG8.addEventListener("click", toggle19);
